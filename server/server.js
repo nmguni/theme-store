86b54379e7b23db6d0e0e3d34f5d8ffd4429ff86
Ndumiso Mguni <Ndumiso29@gmail.com>
2020-11-23 13:04:30 -0500

    server typo

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ dotenv.config();
 
 connectDB();
 
-
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -37,10 +36,10 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
